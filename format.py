6ab64a2bdddd8d536b75cee6674fc938d4af8b37
nikhilsardana <2018nsardana@tjhsst.edu>
2019-06-18 21:13:16 -0400

    finds words, need display

---
+++ format.py
@@ -0,0 +1,31 @@
+import sys
+import re
+
+args = len(sys.argv) - 1
+numchar = 0
+
+if(args!=1):
+	print("This script requires one argument: number of characters per row.")
+	sys.exit(2)
+elif (args==1):
+	try:
+		numchar = int(sys.argv[1])
+	except ValueError:
+		print("Please input a valid integer.")
+		sys.exit(2)
+
+print(numchar)
+
+with open("input.txt") as f:
+	letters = f.readlines()
+
+letters = [x.strip() for x in letters]
+letters = ''.join(letters)
+letters = ''.join(letters.split())
+
+formatted = re.sub("(.{%d})"%numchar, "\\1\n", letters, 0, re.DOTALL)
+print(formatted)
+
+f = open("formatted.txt", "w")
+f.write(formatted)
+f.close()
--- wordsearch.py
+++ wordsearch.py
@@ -1,11 +1,77 @@
 import numpy as np
 import math
-import cv2
-import sys
-import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+def forward(grid, startx, starty, wl, numr, numc):
+  wend = startx+wl-1
+  if(wend>=numr):
+    return
+  else:
+    return ''.join(grid[starty][startx:wend+1])
 
+def backward(grid, startx, starty, wl, numr, numc):
+  wend = startx-wl+1
+  if(wend<0):
+    return
+  else:
+    return ''.join(grid[starty][wend:startx+1])[::-1]
+
+def diag(grid, startx, starty, wl, numr, numc):
+  lst = [""]*4
+  leftx = startx-wl+1
+  rightx = startx+wl-1
+  upy = starty-wl+1
+  downy = starty+wl-1
+  if(leftx>=0 and upy>=0):
+    for i in range(0, wl):
+        lst[0]+=grid[startx-i][starty-i]
+
+  if(leftx>=0 and downy<numc):
+    for i in range(0, wl):
+        lst[1]+=grid[startx-i][starty+i]
+
+  if(rightx<numr and upy>=0):
+    for i in range(0, wl):
+        lst[2]+=grid[startx+i][starty-i]
+
+  if(rightx<numr and downy<numc):
+    for i in range(0, wl):
+        lst[3]+=grid[startx+i][starty+i]
+
+  return lst
+
+def getwords(grid, minw, maxw, numr, numc):
+  lst  = []
+  coord = []
+  for k in range(minw, maxw+1):
+    for i in range(numcols):
+      for j in range(numrows):
+
+        out, c = forward(grid, i, j, k, numr, numc)
+        if not (out==None):
+          lst.append(out)
+          coord.append(c)
+
+        out, c = backward(grid, i, j, k, numr, numc)
+        if not (out==None):
+          lst.append(out)
+          coord.append(c)
+  return lst
+
+def diagwords(grid, minw, maxw, numr, numc):
+  lst = []
+  for k in range(minw, maxw+1):
+    for i in range(numcols):
+      for j in range(numrows):
+        out = diag(grid, i, j, k, numr, numc)
+        for q in out:
+          if not (q==""):
+            lst.append(q)
+
+  return lst
+
+#############################
+# Create Dictionary
+#############################
 
 d = {}
 with open("english.txt") as f:
@@ -19,28 +85,30 @@ with open("addendum.txt") as f:
 		d[key[0].rstrip()] = 1
 
 
+with open("formatted.txt") as f:
+  letters = f.readlines()
 
- 
-if __name__ == '__main__':
- 
-  if len(sys.argv) < 2:
-    print('Usage: python wordsearch.py image.jpg')
-    sys.exit(1)
-   
-  # Read image path from command line
-  imPath = sys.argv[1]
-  
- 
-  # Define config parameters.
-  # '-l eng'  for using the English language
-  # '--oem 1' for using LSTM OCR Engine
-  config = ('-l eng --oem 0 --psm 3')
- 
-  # Read image from disk
-  im = cv2.imread(imPath, cv2.IMREAD_COLOR)
- 
-  # Run tesseract OCR on image
-  text = pytesseract.image_to_string(im, config=config)
- 
-  # Print recognized text
-  print(text)
+letters = [x.rstrip() for x in letters]
+grid = [list(x) for x in letters]
+print(grid)
+print("-----------------------------------")
+gridT = [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))] 
+print(gridT)
+
+minw = 4
+maxw = 10
+numrows = len(grid)
+numcols = len(grid[0])
+
+fb = getwords(grid, minw, maxw, numrows, numcols)
+ud = getwords(gridT, minw, maxw, numcols, numrows)
+di = diagwords(grid, minw, maxw, numrows, numcols)
+
+full = fb+ud+di
+
+found = []
+for word in full:
+  if word.lower() in d:
+    found.append(word)
+
+print(found)
